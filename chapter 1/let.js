de41820c0df6f7a689d5ce797a0d7664689b3e85
With-chomok <183892208+With-chomok@users.noreply.github.com>
2024-12-09 21:24:01 +0600

    let and var different

--- chapter 1/let.js
+++ chapter 1/let.js
@@ -1,4 +1,37 @@
 let x = 10;
 if (x == 10) {
+    
     console.log(x)
+    x = 20;
 }
+console.log(x)
+
+var a = 10;
+var a = 40;
+var a = 50;
+console.log(a);
+
+let b = 40;
+
+console.log(b);
+
+
+console.log(i);
+var i;
+i = 7;
+
+h = 7;
+let = h;
+
+console.log(h); //hoisting use var
+
+let g = 20;
+ g =70;
+ console.log(g); //reassign
+ 
+
+ const k =60;
+
+    console.log(k);
+    
+ 
