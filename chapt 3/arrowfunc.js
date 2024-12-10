3c42645e4a8955256bc869e6dd3c94a3cd21ea56
With-chomok <183892208+With-chomok@users.noreply.github.com>
2024-12-10 22:51:53 +0600

    call function done

---
+++ chapt 3/arrowfunc.js
@@ -0,0 +1,23 @@
+// JavaScript Arrow Functions
+
+let fun = function (a, b){
+    return console.log(a + b);
+}
+fun(20 + 40);
+
+let arrowfun = (c,d) => c + d;
+console.log(arrowfun(40,30));
+
+// advance js function...arrowfun......arrowfun.apply.apply.apply.apply.apply.length
+
+let Function = (a,c,s,g,f,t,r,e) => {
+
+    console.log();
+
+}
+console.log(Function.length);
+
+
+let Func = (a,b,e,r,t,y,u,i,k,j,f,d) => {        
+}
+console.log(Func.prototype); 
---
+++ chapt 3/callfunc.js
@@ -0,0 +1,14 @@
+const person = {
+    fullName: function(){
+        return this.firstName + " " + this.lastName;
+    }
+};
+
+
+const person2 = {
+    firstName: "dipol",
+    lastName:"das"
+}
+
+const valueAdd = person.fullName.call(person2);
+console.log(valueAdd);
