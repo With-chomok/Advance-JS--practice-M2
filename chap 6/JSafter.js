f6d9a407785ac3fa6ef992b25c9d19e9732be6ff
With-chomok <183892208+With-chomok@users.noreply.github.com>
2024-12-19 21:13:24 +0600

    prepend method and append method upload

--- chap 6/JSafter.js
+++ chap 6/JSafter.js
@@ -1,10 +1,21 @@
-const info =document.querySelector(".method");
+// "use strict"
+// const info =document.querySelector(".method");
 
-const h1 =document.createElement("h1");
-h1.textContent = "this is H1 tag";
+// const h1 =document.createElement("h1");
+// h1.textContent = "this is H1 tag";
 
-info.after(h1);
-
-
+// info.after(h1);
 
 
+
+const myInfo = document.querySelector(".method");
+
+const infoList = ["apple", "Orange", "Banana", "Mango", "Cocunut"];
+
+const nodes = infoList.map((para) => {
+    const li = document.createElement("li");
+    li.textContent =para;
+    return li;
+})
+
+myInfo.prepend(...nodes)
