ed3363e4df13202f1ac7554064b66573fcd3a2b3
With-chomok <183892208+With-chomok@users.noreply.github.com>
2024-12-20 21:46:56 +0600

    offset hwight and width

--- chap 6/child.js
+++ chap 6/child.js
@@ -1,8 +1,22 @@
 // 
 // element insrt
 // ----------------------------------------------------------------
-let app = document.querySelector(".app");
-let li = document.createElement("li");
-li.textContent = "home";
+// let app = document.querySelector(".app");
+// let li = document.createElement("li");
+// li.textContent = "home";
 
-app.insertBefore(li, app.lastElementChildElementChild)
+// app.insertBefore(li, app.lastElementChildElementChild)
+
+// get attribute
+// -------------------------------------------------------------
+
+// const menu = document.querySelector("#id");
+
+// const href = menu.getAttribute("id");
+// console.log(href);
+
+let atribute = document.querySelector(".atribute");
+
+let width = atribute.offsetWidth;
+let height = atribute.offsetHeight;
+console.log(width, height)
